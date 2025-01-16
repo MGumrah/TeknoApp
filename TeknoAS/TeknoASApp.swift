98f470060f3f4e3f6a5e42b5f9c365c3d5c42c20
Mehmet Gümrah <mehmetgumrah93@gmail.com>
2025-01-16 15:56:27 +0300

    Initial Commit

---
+++ TeknoAS/TeknoASApp.swift
@@ -0,0 +1,17 @@
+//
+//  TeknoASApp.swift
+//  TeknoAS
+//
+//  Created by Mehmet Gümrah on 16.01.2025.
+//
+
+import SwiftUI
+
+@main
+struct TeknoASApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
